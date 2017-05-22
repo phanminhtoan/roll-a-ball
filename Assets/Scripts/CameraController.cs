c9404f884458c9f13c371e24d1ff0b18349aa26d
PHAN MINH TOÀN <15520908@ms.uit.edu.vn>
2017-05-22 10:56:53 +0700

    initial base map

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,6 +14,6 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position/2 + offset;
+        transform.position = player.transform.position + offset;
 	}
 }
