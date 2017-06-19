b056a84c4cd1ca604ba202ed54cdddd3a1605bc4
PHAN MINH TOÀN <15520908@ms.uit.edu.vn>
2017-06-19 08:29:12 +0700

    edit map view

---
+++ Assets/Scripts/Capsule1Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Capsule1Controller : MonoBehaviour {
+
+	public Transform[] path;
+    public float speed = 5.0f;
+    public float reachDist = 1.0f;
+    public int currentPoint = 0;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.deltaTime*speed);
+
+        if (dist <= reachDist)
+        {
+            currentPoint++;
+        }
+        if(currentPoint >= path.Length)
+        {
+            currentPoint = 0;
+        }
+    }
+
+    void onDrawGizmos()
+    {
+        if(path.Length > 0)
+            for(int i = 0; i < path.Length; i++)
+            {
+                if(path[i] != null)
+                {
+                    Gizmos.DrawSphere(path[i].position, reachDist);
+                }
+            }
+    }
+
+
+
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     public float speed;
     private int score;
+
     public Text textScore;
+    public Text result;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         score = 0;
         setScore();
+        result.text = "";
     }
 
 	void FixedUpdate ()
@@ -27,10 +31,26 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        if (other.CompareTag("Capsule1"))
+        {
+           
+            //Debug.Log("Game over");
+            result.text = "Game over";
+            Time.timeScale = 0;
+           
+        }
         if (other.CompareTag("Pick up"))
             other.gameObject.SetActive(false);
+        
+
         score++;
         setScore();
+
+        if(score == 16)
+        {
+            result.text = "You win";
+            Time.timeScale = 0;
+        }
     }
 
 
